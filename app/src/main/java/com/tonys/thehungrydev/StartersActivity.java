eaf0fe549047fcbfd10799a1b9804f587ffe860e
Minervus <tnguyen411@gmail.com>
2023-10-01 13:02:40 -0700

    added an array of dishes to StartersActivity

--- app/src/main/java/com/tonys/thehungrydev/StartersActivity.java
+++ app/src/main/java/com/tonys/thehungrydev/StartersActivity.java
@@ -2,7 +2,9 @@ package com.tonys.thehungrydev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class StartersActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class StartersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starters);
+
+        ListView startersList = findViewById(R.id.list_view_starters);
+
+        String[] starterDishes = {
+                "Bruschetta",
+                "Caprese Salad",
+                "Caesar Salad",
+                "Spinach and Artichoke Dip",
+                "Shrimp Cocktail",
+                "Stuffed Mushrooms",
+                "Vegetable Spring Rolls",
+                "Chicken Satay",
+                "Calamari",
+                "Potato Skins",
+                "Crab Cakes",
+                "Gazpacho",
+                "Greek Salad",
+                "Prosciutto-wrapped Asparagus",
+                "Deviled Eggs",
+                "Tomato Bisque",
+                "French Onion Soup",
+                "Guacamole",
+                "Hummus",
+                "Clam Chowder"
+        };
+
     }
 }
