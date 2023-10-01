4555192045aabe30b658bcb82f4d9d72778ddc04
Minervus <tnguyen411@gmail.com>
2023-10-01 09:32:46 -0700

    added new StartersActivity

--- app/src/main/java/com/tonys/thehungrydev/MainActivity.java
+++ app/src/main/java/com/tonys/thehungrydev/MainActivity.java
@@ -3,10 +3,13 @@ package com.tonys.thehungrydev;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Adding card views to mainActivity so it can be accessed
     CardView startersCard;
     CardView mainsCard;
 
@@ -14,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startersCard = findViewById(R.id.card_view_starters);
+        startersCard = findViewById(R.id.card_view_starters); //Adding ability to find the card views onCreate
         mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create intent on what we want to do e.g. we want to go to the StartersActivity Class
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent); // Go to this activity
+            }
+        });
+
+
+
     }
 }
